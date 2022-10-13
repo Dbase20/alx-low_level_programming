3cbed1cecd7857d717469fd0fb236a09329001ed
Daniel Etowa <etowadaniel98@gmail.com>
2022-10-13 14:50:53 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
+
+
 /**
  *
- *  * main - entry point
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *   *
+ *   * Return: 0 if exited properly, non-zero otherwise
  *
- *    * Return: Always 0
- *
- *     */
+ *    */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
